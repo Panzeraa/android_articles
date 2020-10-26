4f1101d26ddb24a4b289398742a51905624cff13
Panz <alexpanz06@gmail.com>
2020-10-26 09:02:01 +0100

    gestion erreurs

--- src/main/java/com/mbds/article/repositories/Articlepository.kt
+++ src/main/java/com/mbds/article/repositories/Articlepository.kt
@@ -26,15 +26,14 @@ class Articlepository {
     fun list(categorie: String): List<Article>? {
         val response = service.list(categorie).execute()
         println(response.code())
-//        when(response.code()){
-//            400-> Toast.makeText(context, "Bad Request. The request was unacceptable, often due to a missing or misconfigured parameter.", Toast.LENGTH_SHORT).show()
-//            401-> Toast.makeText(context, "Unauthorized. Your API key was missing from the request, or wasn't correct.", Toast.LENGTH_SHORT).show()
-//            429-> Toast.makeText(context, "Too Many Requests. You made too many requests within a window of time and have been rate limited. Back off for a while.", Toast.LENGTH_SHORT).show()
-//            500-> Toast.makeText(context, "Server Error. Something went wrong on our side.", Toast.LENGTH_SHORT).show()
-//            else-> Toast.makeText(context, "OK. The request was executed successfully.", Toast.LENGTH_SHORT).show()
-//        }
+        if(response.code() != 200){
+            throw Exception(response.code().toString())
+        }
+
+
         return response.body()?.articles
 
     }
+
 
 }
